99081b3e0db4ebd0e1c8f8374756786c45e2227e
neha45556 <neha45556@gmail.com>
2019-11-26 14:30:45 -0800

    Update main.c

--- Piano_project/source/main.c
+++ Piano_project/source/main.c
@@ -484,7 +484,7 @@ void menu(){
 
 int main(void){
   	DDRA = 0x00; PORTA = 0xF8;
- 	DDRB = 0xBF; PORTB = 0x40;
+ 	DDRB = 0x00; PORTB = 0xFF;
 	DDRD = 0xFF; PORTD = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
 	PWM_on();
