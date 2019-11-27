ca4e361c008e17ea7dbb99acfd66251881cbfd04
neha45556 <neha45556@gmail.com>
2019-11-26 16:18:13 -0800

    Update main.c

--- Piano_project/source/main.c
+++ Piano_project/source/main.c
@@ -635,23 +635,63 @@ void sound(){
 			
 // }
 
-int main(){
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	DDRD = 0xFF; PORTD = 0x00;
-	DDRC = 0xFF; PORTC = 0x00;
+// int main(){
+// 	DDRA = 0x00; PORTA = 0xFF;
+// 	DDRB = 0xFF; PORTB = 0x00;
+// 	DDRD = 0xFF; PORTD = 0x00;
+// 	DDRC = 0xFF; PORTC = 0x00;
 
-	PWM_on();
-	HC595Init();
-	state = init;
+// 	PWM_on();
+// 	HC595Init();
+// 	lcd_init(LCD_DISP_ON_BLINK);
+         
+// 	state = init;
 	
-	while(1){
-		HC595Write(0b00000000);
-		sound();
-	}
-	return 1;
-}
+// 	while(1){
+// 		HC595Write(0b00000000);
+// 		sound();
+// 	}
+// 	return 1;
+// }
 
+void InitADC(void)
+{
+    ADMUX|=(1<<REFS0);    
+    ADCSRA|=(1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2); //ENABLE ADC, PRESCALER 128
+}
+uint16_t readadc(uint8_t ch)
+{
+    ch&=0b00000111;         //ANDing to limit input to 7
+    ADMUX = (ADMUX & 0xf8)|ch;  //Clear last 3 bits of ADMUX, OR with ch
+    ADCSRA|=(1<<ADSC);        //START CONVERSION
+    while((ADCSRA)&(1<<ADSC));    //WAIT UNTIL CONVERSION IS COMPLETE
+    return(ADC);        //RETURN ADC VALUE
+}
+int main(void)
+{
+    char a[20], b[20], c[20];   
+    uint16_t x,y,z;
+    InitADC();         //INITIALIZE ADC
+    lcd_init(LCD_DISP_ON);  //INITIALIZE LCD
+    lcd_clrscr();      
+    while(1)
+    {
+        lcd_home();         
+        x=readadc(0);      //READ ADC VALUE FROM PA.0
+        y=readadc(1);      //READ ADC VALUE FROM PA.1
+        x=x-512;
+        y=y-512;
+        itoa(x,a,10);    
+        itoa(y,b,10);
+        lcd_puts("x=");     //DISPLAY THE RESULTS ON LCD
+        lcd_gotoxy(2,0);
+        lcd_puts(a);
+        lcd_gotoxy(7,0);
+        lcd_puts("y=");
+        lcd_gotoxy(9,0);
+        lcd_puts(b);
+    }
+}
 
 
 
