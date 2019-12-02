096dc1784a7e9c9e21d30a596b74085cbd74cd9b
neha45556 <neha45556@gmail.com>
2019-12-02 10:11:36 -0800

    Update main.c

--- Piano_project/source/main.c
+++ Piano_project/source/main.c
@@ -13,6 +13,13 @@ Please refer to LICENSE file for licensing information.
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+
+//#include "io.h"
+#ifdef _SIMULATE_
+#include "/Users/nehagupta/Downloads/simavr-master/simavr/sim/avr/avr_mcu_section.h"
+#endif
+
+
 #include "lcdpcf8574.h"
 
 
@@ -394,34 +401,14 @@ void sound(){
 //     return(ADC);        //RETURN ADC VALUE
 // }
 
-// void ADC_Init()
-// {
-// 	DDRA = 0x00;		/* Make ADC port as input */
-// 	ADCSRA = 0x87;		/* Enable ADC, fr/128  */
-// 	ADMUX = 0x40;		/* Vref: Avcc, ADC channel: 0 */
-// }
 
-// int ADC_Read(char channel)
-// {
-// 	int ADC_value;
-	
-// 	ADMUX = (0x40) | (channel & 0x07);/* set input channel to read */
-// 	ADCSRA |= (1<<ADSC);	/* start conversion */
-// 	while((ADCSRA &(1<<ADIF))== 0);	/* monitor end of conversion interrupt flag */
-	
-// 	ADCSRA |= (1<<ADIF);	/* clear interrupt flag */
-// 	ADC_value = (int)ADCL;	/* read lower byte */
-// 	ADC_value = ADC_value + (int)ADCH*256;/* read higher 2 bits, Multiply with weightage */
-
-// 	return ADC_value;		/* return digital value */
-// }
 
 //enum states {song1,song2} state;
-//void menu(){
+// void menu(){
 // 	uint16_t x,y;
 // 	lcd_puts("SONGS                                                    SONG1");
 // 	x = ADC_Read(0);
-// 	y = ADC_Read(1);
+// 	y = ADC_Read(1);/
 	/*
 	double NOTE_C4 = 261.63;
 	double NOTE_D4 = 293.66;
@@ -466,6 +453,7 @@ int main(void)
 {
 DDRA = 0x00; PORTA = 0xFF;
 DDRB = 0xE0; PORTB = 0x1F;
+DDRC = 0xFF; PORTC = 0x00;
 DDRD = 0xFF; PORTD = 0x00;
 
   
